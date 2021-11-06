71ce5dc88efb00ada6661a7ca67189414154f43b
nowl <kurealkatraz@gmail.com>
2021-11-06 16:51:55 +0100

    feat: added joystick support

--- src/physical_input.c
+++ src/physical_input.c
@@ -1,15 +1,7 @@
 #include "speed.h"
 
-Input *new_input() {
-  Input *input = (Input *)malloc(sizeof(Input));
-
-  input->direction.x = 0.0;
-  input->direction.y = 0.0;
-  input->action = false;
-  return input;
-}
-
-void destroy_input(Input *input) { free(input); }
+#define JOYSTICK_X_AXIS (Uint8)0
+#define JOYSTICK_Y_AXIS (Uint8)1
 
 void handle_keydown(SDL_Keycode keycode, Input *input) {
   switch (keycode) {
@@ -51,6 +43,28 @@ void handle_keyup(SDL_Keycode keycode, Input *input) {
   }
 }
 
+void handle_joystick_motion(Input *input, SDL_JoyAxisEvent event) {
+  switch (event.axis) {
+  case JOYSTICK_X_AXIS:
+    input->direction.x = event.value / SDL_JOYSTICK_AXIS_MAX;
+    break;
+  case JOYSTICK_Y_AXIS:
+    input->direction.y = event.value / SDL_JOYSTICK_AXIS_MAX;
+    break;
+  }
+}
+
+Input *new_input() {
+  Input *input = (Input *)malloc(sizeof(Input));
+
+  input->direction.x = 0.0;
+  input->direction.y = 0.0;
+  input->action = false;
+  return input;
+}
+
+void destroy_input(Input *input) { free(input); }
+
 void update_input(Input *input, SDL_Event *event) {
   switch (event->type) {
   case SDL_KEYDOWN:
@@ -58,6 +72,9 @@ void update_input(Input *input, SDL_Event *event) {
     break;
   case SDL_KEYUP:
     handle_keyup(event->key.keysym.sym, input);
+    break;
+  case SDL_JOYAXISMOTION:
+    handle_joystick_motion(input, event->jaxis);
     break;
   }
 }
